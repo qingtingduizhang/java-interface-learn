9fade3ad3f47cd96961ec13c320d7882b65ebc43
lmf <lmf@zenx.ltd>
2022-10-28 14:06:54 +0800

    抽象类可以实现接口

---
+++ interface-learn/src/main/java/com/wuman/A.java
@@ -0,0 +1,21 @@
+package com.wuman;
+
+/**
+ * 抽象类可以实现接口
+ * @author lmf
+ * @version 1.0
+ * @date 2022/10/28 14:01
+ */
+public abstract class A implements B {
+    @Override
+    public void a() {
+
+    }
+
+    @Override
+    public void b() {
+
+    }
+
+
+}
---
+++ interface-learn/src/main/java/com/wuman/B.java
@@ -0,0 +1,11 @@
+package com.wuman;
+
+/**
+ * @author lmf
+ * @version 1.0
+ * @date 2022/10/28 14:01
+ */
+public interface B {
+    void a();
+    void b();
+}
